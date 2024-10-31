fd2982a4ad20abad243761036847e88b1a2e6b63
Dabwitso Maweemba <mweembadabwitso@gmail.com>
2024-11-01 01:53:54 +0200

    Added views for the services provided by the salon

--- services/views.py
+++ services/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
+from .models import Service
 
-# Create your views here.
+class ServiceListView(ListView):
+    """
+    View to list all services.
+    """
+    model = Service
+    template_name = "services/service_list.html"
+    context_object_name = "services"
+
+
+class ServiceDetailView(DetailView):
+    """
+    View to display details of a single service.
+    """
+    model = Service
+    template_name = "services/service_detail.html"
+    context_object_name = "service"
+
+
+class ServiceCreateView(CreateView):
+    """
+    View to create a new service.
+    """
+    model = Service
+    fields = ['name', 'description', 'price', 'duration']
+    template_name = "services/service_form.html"
+    success_url = reverse_lazy('service_list')
+
+
+class ServiceUpdateView(UpdateView):
+    """
+    View to update an existing service.
+    """
+    model = Service
+    fields = ['name', 'description', 'price', 'duration']
+    template_name = "services/service_form.html"
+    success_url = reverse_lazy('service_list')
