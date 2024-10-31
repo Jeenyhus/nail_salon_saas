95e87d89fc4b9420570b7e5b43d4a6cb086de9f7
Dabwitso Maweemba <mweembadabwitso@gmail.com>
2024-11-01 01:35:09 +0200

    Updated Customer app mdoel registrations

--- customers/admin.py
+++ customers/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Customer, CustomerInteraction, CustomerLoyalty
 
 # Register your models here.
 admin.site.register(Customer)
+admin.site.register(CustomerInteraction)
+admin.site.register(CustomerLoyalty)
